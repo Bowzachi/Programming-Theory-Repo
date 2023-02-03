add568b287a934f71bfe44ee6f8bb4feec73f75c
Bowzachi <67884554+Bowzachi@users.noreply.github.com>
2023-02-03 01:34:42 -0800

    setup player and fish
    player and fish in main scene for testing and converting to prefabs.

---
+++ Programming Theory/Assets/Scripts/Fish.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    private bool isTagged = false;
+    public float speed = 10f;
+    public float speedMultiplier = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //movement code calling speed and speedMultiplier
+    }
+}
